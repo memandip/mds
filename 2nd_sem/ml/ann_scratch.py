e080b98d3ab57bf5e825dfeee9aa107650182a51
Mandip Chaudhary <mandip810@gmail.com>
2025-12-06 17:47:33 +0545

    feat: Initial ANN structure

---
+++ 2nd_sem/ml/ann_scratch.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+class ANN:
+    def __init__(self, input_size, hidden_size, output_size):
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+
+        # Initialize weights and biases
+        self.weights_input_hidden = np.random.randn(self.input_size, self.hidden_size)
+        self.bias_hidden = np.zeros((1, self.hidden_size))
+        self.weights_hidden_output = np.random.randn(self.hidden_size, self.output_size)
+        self.bias_output = np.zeros((1, self.output_size))
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def forward(self, X):
+        # Input to hidden layer
+        self.hidden_input = np.dot(X, self.weights_input_hidden) + self.bias_hidden
+        self.hidden_output = self.sigmoid(self.hidden_input)
+
+        # Hidden to output layer
+        self.output_input = np.dot(self.hidden_output, self.weights_hidden_output) + self.bias_output
+        self.output = self.sigmoid(self.output_input)
+        return self.output
